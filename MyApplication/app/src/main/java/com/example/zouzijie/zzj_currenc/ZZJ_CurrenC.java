aceb519a1b56306556d3204d86b22944cec460ed
Zijie ZOU <zouzi.jie@163.com>
2017-03-02 00:09:52 +0800

    Create

---
+++ MyApplication/app/src/main/java/com/example/zouzijie/zzj_currenc/ZZJ_CurrenC.java
@@ -0,0 +1,13 @@
+package com.example.zouzijie.zzj_currenc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ZZJ_CurrenC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zzj__curren_c);
+    }
+}
